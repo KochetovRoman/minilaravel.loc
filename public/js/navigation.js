144bca352033ad1e55a27aa46f595c8c4cc93015
KochetovRoman <titanium.rrr@gmail.com>
2018-05-29 13:57:49 +0300

    first commit

---
+++ public/js/navigation.js
@@ -0,0 +1,15 @@
+$(function () {
+    var location = window.location.href;
+    var cur_url = '/' + location.split('/').pop();
+
+    $('#navigation li').each(function () {
+        var link = $(this).find('a').attr('href');
+
+        if (cur_url == link)
+        {
+            $(this).addClass('active');
+        }
+
+    });
+
+});
